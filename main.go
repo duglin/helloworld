93415a0dfdf8015bf52a4c4828ec6d1f1c511be4
Doug Davis <dug@us.ibm.com>
2019-01-22 04:49:23 -0800

    env
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	ending := os.Getenv("ENDING")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
-		fmt.Fprintf(w, "Hello world!%s\n", ending)
+		fmt.Fprintf(w, "Hello world! %s\n", ending)
 	})
 
 	fmt.Print("Listening on port 8080\n")
