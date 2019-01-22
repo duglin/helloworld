6619c956df91cef999ec32491211190fc423df81
Doug Davis <dug@us.ibm.com>
2019-01-21 20:29:55 -0800

    nosleep
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// body, _ := ioutil.ReadAll(r.Body)
 		// fmt.Printf("URL: %s\nBody: %v\n", r.URL, string(body))
-		time.Sleep(2 * time.Second)
 		fmt.Fprintf(w, "Hello world!\n")
 	})
 
