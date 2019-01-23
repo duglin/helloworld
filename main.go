695f4de142d5e5e3f9447015a58829e967550465
Doug Davis <dug@us.ibm.com>
2019-01-23 09:09:55 -0800

    demo - Wed Jan 23 09:09:52 PST 2019
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Got request\n")
 		time.Sleep(200 * time.Millisecond)
-		fmt.Fprintf(w, "Hello World! 29964\n")
+		fmt.Fprintf(w, "Hello World! 1270\n")
 	})
 
 	fmt.Print("Listening on port 8080\n")
