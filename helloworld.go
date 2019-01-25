c6197ef18a221ef8745c2ec942fd1d74ccfeb88c
Doug Davis <dug@us.ibm.com>
2019-01-25 06:38:14 -0800

    demo - Fri Jan 25 06:38:13 PST 2019
    Signed-off-by: Doug Davis <dug@us.ibm.com>

---
+++ helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Got request\n")
+		time.Sleep(200 * time.Millisecond)
+		fmt.Fprintf(w, "Hello World! 5976\n")
+	})
+
+	fmt.Print("Listening on port 8080\n")
+	http.ListenAndServe(":8080", nil)
+}
