0b5984226b06277834d3bfd33fa8f6991f3c474d
Doug Davis <dug@us.ibm.com>
2019-02-03 06:31:32 -0800

    clean
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- helloworld.go
+++ helloworld.go
@@ -8,11 +8,17 @@ import (
 )
 
 func main() {
+	text := "Hello World!"
+	rev := os.Getenv("K_REVISION")
+	if rev != "" {
+		rev = rev[11:]
+	}
+	msg := fmt.Sprintf("%s: %s\n", rev, text)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Got request\n")
 		time.Sleep(200 * time.Millisecond)
-		text := "Hello World!"
-		fmt.Fprintf(w, os.Getenv("K_REVISION")+": "+text+"\n")
+		fmt.Fprint(w, msg)
 	})
 
 	fmt.Print("Listening on port 8080\n")
