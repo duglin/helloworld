269bdf39364e084dd546ab0d6153303db80d92d3
Doug Davis <dug@us.ibm.com>
2019-02-14 07:15:05 -0800

    demo - Thu Feb 14 07:15:04 PST 2019
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- helloworld.go
+++ helloworld.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	text := "Hello World!"
+	text := "H World!"
 
 	rev := os.Getenv("K_REVISION") // K_REVISION=helloworld-00001
 	if i := strings.LastIndex(rev, "-"); i > 0 {
